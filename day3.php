8eefb533bb9343706060335053cfcaeefd2d04eb
Alex <akveltman@gmail.com>
2018-09-25 16:50:05 -0500

    change for b1

--- day3.php
+++ day3.php
@@ -22,6 +22,7 @@ Data from square 1024 must be carried 31 steps.
 How many steps are required to carry the data from the square identified in your puzzle input all the way to the access port?
 1 8 16 24
 
+First change here
  */
 $buildsqare = array();
 
